0cff6778d903a8110eece963229ee0b6e3cb7a5f
chrysanthemum <asingiser@gmail.com>
2024-03-19 02:17:54 -0400

    work through tasks

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,9 @@
 export default function Banner() {
     return (
         <div>
-            <header>
+            <h1>
                 Orbit Report
-            </header>
+            </h1>
             <p>
                 Click on the buttons to see the satellites in that orbit type
             </p>
